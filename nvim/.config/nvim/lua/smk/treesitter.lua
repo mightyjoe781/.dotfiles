704527e9aa19f6d2ed128f0b2c0f4055fe60e5fc
smk <sudhanshumohan781@gmail.com>
2022-07-04 20:27:00 +0530

    treesitter update

--- nvim/.config/nvim/lua/smk/treesitter.lua
+++ nvim/.config/nvim/lua/smk/treesitter.lua
@@ -1,14 +1,23 @@
-local treesitter = require('nvim-treesitter.configs')
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    return
+end
 
-treesitter.setup {
+configs.setup {
+    -- ensure_installed = "maintained", -- one of "all", "maintained"
+    sync_install = false, -- install languages synchronously (only applied to ensure_installed)
+    ignore_install = { "" }, -- list of parsers to ignore installing
     highlight = {
-        enable = true
+        enable = true,
+        disable = { "" },
+        additional_vim_regex_highlighting = true,
     },
     incremental_selection = {
         enable = true
     },
     textobjects = {
         enable = true
-    }
+    },
+    indent = { enable = true, disable = { "yaml" } },
 }
 
